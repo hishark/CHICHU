fa039c1b79f48b830ef766b4e5464eedf9330845
hishark <hishark@yeah.net>
2018-03-18 14:30:44 +0800

    first commit test

--- myproject/src/main/java/com/example/myapplication/activity/ChatActivity.java
+++ myproject/src/main/java/com/example/myapplication/activity/ChatActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.myapplication.R;
 
@@ -11,5 +12,10 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        /*
+         * 14点28分 试一下
+         */
+        TextView tv;
     }
 }
